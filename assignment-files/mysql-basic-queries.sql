ec71d1731c3b0fc702e7ad4fe0ab78f72657ca5b
tphdev <tphdevdrop@gmail.com>
2018-07-19 19:22:37 -0500

    creating assignment file for mysql

---
+++ assignment-files/mysql-basic-queries.sql
@@ -0,0 +1,32 @@
+
+# [Example]
+# 1 Write a query to display the full name (FIRST_NAME + LAST_NAME) for all employees whose first name is 'John'.
+
+SELECT FIRST_NAME, LAST_NAME
+FROM Employees
+WHERE
+  FIRST_NAME = 'John';
+
+# [Your Work Below]
+
+# 1 Write a query to display the full name (FIRST_NAME + LAST_NAME) and SALARY for all employees whose salary that is greater than $15,000.
+
+# 2 Write a query to display the full name (FIRST_NAME + LAST_NAME) and DEPARTMENT_ID of all employees in departments 30 or 100 in ascending order.
+
+# 3 Write a query to display the full name (FIRST_NAME + LAST_NAME) and SALARY for all employees whose salary is not in the range $10,000 through $15,000 and are in department 30 or 100.
+
+# 4 Write a query to display the full name (FIRST_NAME + LAST_NAME) and HIRE_DATE for all employees who were hired in 1987.
+
+# 5 Write a query to display the first name of all employees who have both "b" and "c" in their first name.
+
+# 6 Write a query to display the last name, job, and salary for all employees whose job is that of a Programmer (IT_PROG) or a Shipping Clerk (SH_CLERK), and whose salary is not equal to $4,500, $10,000 or $15,000.
+
+# 7 Write a query to display the last name of employees whose names have exactly 6 characters.
+
+# 8 Write a query to display the last name of employees having 'e' as the third character.
+
+# 9 Write a query to display the JOBS available in the employees table.
+
+# 10 Write a query to display the full name (FIRST_NAME, LAST_NAME), SALARY and PF (15% of salary) of all employees.
+
+# 11 Write a query to select all record from employees where last name in 'BLAKE', 'SCOTT', 'KING' and 'FORD'.
